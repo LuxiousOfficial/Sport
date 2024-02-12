d544ed720cd2abf712365e51ccfef302aa071d43
Rommy Ardiansyah <136146345+LuxiousOfficial@users.noreply.github.com>
2024-02-12 14:07:01 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,24 @@ document.addEventListener('click', function(e) {
 	}
 });
 
+// Togle class active search-form //
+
+	const searchForm = document.querySelector('.search-form');
+	const searchBox = document.querySelector('#search-box');
+
+	document.querySelector('#search-button').onclick = (e) => {
+		searchForm.classList.toggle('active');
+		e.preventDefault();
+	};
+
+	const sb = document.querySelector('#search-button');
+
+	document.addEventListener('click', function(e) {
+		if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+			searchForm.classList.remove('active');
+		}
+	});
+
 ScrollReveal({
 	reset:true, 
 	distance: '100px',
